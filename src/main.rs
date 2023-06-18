8b53789327192ee522320e0bab7a019534dcbfce
Calco Dev <calcopoddev@gmail.com>
2023-06-19 02:07:24 +0300

    Implemented basic health_check.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::http::StatusCode;
+use actix_web::{
+    get, App, HttpResponseBuilder, HttpServer, Responder,
+};
+
+#[get("/health_check")]
+async fn health_check() -> impl Responder {
+    HttpResponseBuilder::new(StatusCode::OK)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| App::new().service(health_check))
+        .bind("127.0.0.1:8000")
+        .unwrap()
+        .run()
+        .await
 }
