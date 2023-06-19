7a05c69a4f24f2130eda1434fdceeab33663f1af
Calco Dev <calcopoddev@gmail.com>
2023-06-20 00:08:33 +0300

    Loading configuration from JSON.

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,4 +1,37 @@
 ﻿use serde::Deserialize;
 
 #[derive(Deserialize)]
-pub struct Settings {}
+pub struct Settings {
+    pub application_port: u16,
+    pub db_settings: DatabaseSettings,
+}
+
+#[derive(Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub db_name: String,
+}
+
+impl DatabaseSettings {
+    pub fn get_connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.db_name
+        )
+    }
+}
+
+pub fn get_config() -> Result<Settings, config::ConfigError> {
+    let settings = config::Config::builder()
+        .add_source(config::File::new(
+            "configuration.json",
+            config::FileFormat::Json5,
+        ))
+        .build()
+        .unwrap();
+
+    settings.try_deserialize::<Settings>()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use inkwell_api::configuration::get_config;
 use inkwell_api::run;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
-
+    let config = get_config().expect("Failed to read configuration");
+    
+    let host = "127.0.0.1";
+    let addr = format!("{}:{}", host, config.application_port);
+    let listener = TcpListener::bind(addr).unwrap();
+    
     let server = run(listener).unwrap();
     server.await
 }
